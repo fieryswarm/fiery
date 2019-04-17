a292e50f9d7378c062c276ce1c06cbc786cad53a
David Feinzimer <d@fein.io>
2019-04-16 20:59:04 -0700

    04.16.2019.20.59.03

--- core/turnaround.ino
+++ core/turnaround.ino
@@ -5,11 +5,11 @@ const int resolution = 8;
 // Turning motor properties
 int turn_PWM = 14;  // adc2_channel 8
 int turn_enable  = 27;
-int turn_right1_left0 = 12;
+int turn_direction = 12;
 const int turnChannel = 8;
 
 // Driving motor properties
-int drive_forward_reverse = 26;
+int drive_direction = 26;
 int drive_PWM = 13;   // adc1_channel 5
 int drive_enable = 25;
 const int driveChannel = 5;
@@ -24,29 +24,72 @@ void setup() {
 
     pinMode(turn_enable, OUTPUT);
     pinMode(turn_PWM, OUTPUT);
-    pinMode(turn_right1_left0, OUTPUT);
+    pinMode(turn_direction, OUTPUT);
 
     pinMode(drive_enable, OUTPUT);
     pinMode(drive_PWM, OUTPUT);
-    pinMode(drive_forward_reverse, OUTPUT);
-
-    digitalWrite(turn_enable, HIGH);
-    digitalWrite(drive_enable,  HIGH); //
-
-    digitalWrite(turn_right1_left0, LOW);
-    digitalWrite(drive_forward_reverse, LOW);
-
+    pinMode(drive_direction, OUTPUT);
 
     ledcSetup(driveChannel, freq, resolution);
     ledcSetup(turnChannel, freq, resolution);
 
-    // attach the channel to the GPIO to be controlled
+    // Attach the channel to the GPIO to be controlled
     ledcAttachPin(turn_PWM, turnChannel);
     ledcAttachPin(drive_PWM, driveChannel);
+
+    // Enable all motors
+    digitalWrite(turn_enable, HIGH);
+    digitalWrite(drive_enable,  HIGH);
 }
 
+void turnaround() {
+  Serial.println("Start in a stopped state");
+  ledcWrite(driveChannel, 0);
+  ledcWrite(turnChannel, 0);
+
+  Serial.println("Prepare wheels for right turn");
+  digitalWrite(turn_direction, LOW);    // Right:     L   Left:     H
+
+  Serial.println("Prepare wheels for reverse");
+  digitalWrite(drive_direction, HIGH);   // Forward:   L   Reverse:  H
+
+  Serial.println("Apply power to turning motors");
+  ledcWrite(turnChannel, 120);
+
+  Serial.println("Apply power to drive motors");
+  ledcWrite(driveChannel, 120);
+
+  Serial.println("Run all motors for 3 seconds");
+  delay(3);
+
+  Serial.println("Kill all motors");
+  ledcWrite(driveChannel, 0);
+  ledcWrite(turnChannel, 0);
+
+  Serial.println("Prepare wheels for left turn");
+  digitalWrite(turn_direction, HIGH);    // Right:     L   Left:     H
+
+  Serial.println("Prepare wheels for forward");
+  digitalWrite(drive_direction, LOW);   // Forward:   L   Reverse:  H
+
+  Serial.println("Apply power to turning motors");
+  ledcWrite(turnChannel, 120);
+
+  Serial.println("Apply power to drive motors");
+  ledcWrite(driveChannel, 120);
+
+  Serial.println("Run all motors for 3 seconds");
+  delay(3);
+
+  Serial.println("Kill all motors");
+  ledcWrite(driveChannel, 0);
+  ledcWrite(turnChannel, 0);
+}
 
 void loop() {
-  ledcWrite(driveChannel, 120);
+  Serial.println("turnaround()");
+  turnaround();
+
+  Serial.println("Wait 10 seconds for next run");
   delay(10);
 }
